feb654a924d42f7a0c295799de292e655b51f15c
yohan <koyohanyoko@gmail.com>
2023-08-01 21:39:11 +0900

    react-hooks DAY 1

---
+++ DAY 1/useInput.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const useInput = (initialValue, vaildator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if(typeof vaildator === "function"){
+      willUpdate = vaildator(value);
+    }
+    if (willUpdate){
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
